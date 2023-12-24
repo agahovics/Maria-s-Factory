0f41f2d212bfe746d855b91b0611c267fad02fc3
agahovics <agahkrgz@gmail.com>
2023-12-25 00:32:25 +0300

    header and home section completed

---
+++ app.js
@@ -0,0 +1,9 @@
+let search = document.querySelector('.search');
+document.querySelector('#search').onclick=() => {
+    search.classList.toggle('active');
+}
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() => {
+    navbar.classList.toggle('active');
+}
